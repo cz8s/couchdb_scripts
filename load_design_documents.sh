2670898943100264ecb54bfd891cfca9d9e0bc22
Micah Anderson <micah@riseup.net>
2013-11-27 16:37:57 -0500

    fix quoting

--- load_design_documents.sh
+++ load_design_documents.sh
@@ -4,5 +4,5 @@ for file in `find /srv/leap/couchdb/designs -type f -name \*.json`
 do
     db=${file%/*}
     db=${db##*/}
-    "/usr/local/bin/couch-doc-update --host 127.0.0.1:5984 --db $db --id _security --data '{}' --file $file"
+    /usr/local/bin/couch-doc-update --host 127.0.0.1:5984 --db $db --id _security --data '{}' --file $file
 done
