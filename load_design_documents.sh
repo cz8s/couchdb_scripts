ddccc20f2f1ed366995ce9fadc4ec5ae8a193590
Micah Anderson <micah@riseup.net>
2013-11-27 10:45:10 -0500

    change parsing of design documents to work with a layout that would allow for a naming scheme where the directory name is the database name, and the file name is the design doc name:
    eg. users/User.json should end up in users/_design/User
    
    This will allow us to use different design docs per database for different purposes.
    
    eg. users/_design/User for the data schema and views
    and users/_design/write_restricted for validations that prevent certain couch users from writing
    and users/_design/filters for filters that filter the changes stream for things like tapicero to only receive the relevant changes.

--- load_design_documents.sh
+++ load_design_documents.sh
@@ -2,6 +2,7 @@
 
 for file in `find /srv/leap/couchdb/designs -type f -name \*.json`
 do
-    db=`basename $file .json`
-    /usr/local/bin/couch-doc-update --host 127.0.0.1:5984 --db $db --id _security --data '{}' --file $file
+    db=${file%/*}
+    db=${db##*/}
+    echo "/usr/local/bin/couch-doc-update --host 127.0.0.1:5984 --db $db --id _security --data '{}' --file $file"
 done
